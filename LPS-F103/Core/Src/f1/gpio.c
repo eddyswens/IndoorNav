75e04e2d8c08431fbda329efa4ddab85e06867f2
e.pyhalov <user@spbhws376-ubuntu.geoscan.lan>
2024-08-06 18:20:28 +0300

    Fixed wrong gpio config

--- LPS-F103/Core/Src/f1/gpio.c
+++ LPS-F103/Core/Src/f1/gpio.c
@@ -77,24 +77,19 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PC7 - DWM1000 RSTn*/
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, 1);
-
   GPIO_InitStruct.Pin = GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, 1);
 
-   GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+  /*Configure GPIO pin : PC0, PC1, PC2,  - LEDs*/
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
-
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, 1);
-  
-  
- 
-  
 }
 
 /* USER CODE BEGIN 2 */
